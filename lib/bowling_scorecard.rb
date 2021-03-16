fe57dc756e7694755a1143a5b6ca1a8b42537499
Eds <steadyede@live.co.uk>
2021-03-16 08:55:25 +0000

    rubocopped

--- lib/bowling_scorecard.rb
+++ lib/bowling_scorecard.rb
@@ -1,36 +1,38 @@
-class Bowling_Scorecard
+# frozen_string_literal: true
+
+# Runs game of bowling through user inputs into record_roll method
+class BowlingScorecard
   attr_reader :frame, :scorecard
 
   def initialize
     @frame = 1
     @rolls_in_frame = 0
-    @scorecard = {1 => []}
+    @scorecard = { 1 => [] }
     @bonus_queue = {}
   end
 
   def record_roll(input)
-    raise "Game is finished!" if @scorecard[10] && @scorecard[10].length == 3 
+    raise 'Game is finished!' if @scorecard[10] && @scorecard[10].length == 3
 
-    @rolls_in_frame += 1 
+    @rolls_in_frame += 1
     process_bonuses(input)
     start_new_frame(1) if frame_finished_no_bonus? # records input as first roll of new frame
 
     @scorecard[@frame] << input
-    return "Perfect Game!" if total_score == 300
-    
+    return 'Perfect Game!' if total_score == 300
+
     process_spares
     process_strikes
 
     if frame_ten_no_bonus?
       @scorecard[10] << 0
-      return "Gutter Game!" if total_score == 0
+      return 'Gutter Game!' if total_score.zero?
     end
-
   end
 
   def process_spares
-    if spare? 
-      add_to_bonus_queue(@frame, :spare, 1) unless @frame == 10 
+    if spare?
+      add_to_bonus_queue(@frame, :spare, 1) unless @frame == 10
       start_new_frame(0) unless @frame == 10
     elsif spare? && @frame == 10
       add_to_bonus_queue(@frame, :spare, 0)
@@ -39,56 +41,59 @@ class Bowling_Scorecard
   end
 
   def process_strikes
-    if strike?
+    if strike? && @frame == 10
+      process_strike_frame_ten
+    elsif strike? 
       add_to_bonus_queue(@frame, :strike, 2) unless @frame == 10
       start_new_frame(0) unless @frame == 10
-    elsif strike? && @frame == 10
-      add_to_bonus_queue(@frame, :strike, 0)
-      @rolls_in_frame = 0 # to allow for another strike
     end
+  end
+  
+  def process_strike_frame_ten
+    add_to_bonus_queue(@frame, :strike, 0)
+    @rolls_in_frame = 0 # to allow for another strike
   end
 
   def process_bonuses(bonus_score)
-    queue = @bonus_queue.select {|k,v| v[:rolls_left] > 0} # filter queue where rolls left > 0
+    queue = @bonus_queue.select { |_k, v| (v[:rolls_left]).positive? } # filter queue where rolls left > 0
     # add bonus, reduce rolls left to process
-    queue.each do |frame, values|
+    queue.each do |frame, _values|
       @scorecard[frame] << bonus_score
       @bonus_queue[frame][:rolls_left] -= 1
     end
-
   end
 
   def add_to_bonus_queue(frame, scenario, rolls_left)
     @bonus_queue[frame] = { scenario: scenario, rolls_left: rolls_left }
   end
-  
+
   def start_new_frame(rolls)
-    @frame += 1 
+    @frame += 1
     @scorecard[@frame] = []
-    @rolls_in_frame = rolls 
+    @rolls_in_frame = rolls
   end
 
-  
   def frame_score(frame_number)
     raise "Score for #{frame_number} unavailable" unless @scorecard[frame_number]
-    @scorecard[frame_number].inject(0){| sum, x | sum + x } # sums two scores in the frame
+
+    @scorecard[frame_number].inject(0) { |sum, x| sum + x } # sums two scores in the frame
   end
-  
+
   def total_score
     sum = 0
-    @scorecard.each do |frame, score|
+    @scorecard.each do |_frame, score|
       score.each do |value|
         sum += value # sum of arrays
       end
     end
     sum
   end
-  
+
   def spare?
-    @rolls_in_frame == 2 && frame_score(@frame) == 10 && @scorecard[@frame][0] != 10 
+    @rolls_in_frame == 2 && frame_score(@frame) == 10 && @scorecard[@frame][0] != 10
     # check that first score of frame wasnt 10
   end
-  
+
   def strike?
     @rolls_in_frame == 1 && frame_score(@frame) == 10
   end
@@ -100,5 +105,4 @@ class Bowling_Scorecard
   def frame_ten_no_bonus?
     @rolls_in_frame == 2 && @frame == 10 && frame_score(10) < 10
   end
-
 end
--- spec/bowling_scorecard_spec.rb
+++ spec/bowling_scorecard_spec.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'bowling_scorecard'
 
-describe Bowling_Scorecard do
+describe BowlingScorecard do
   it "can record a user's roll" do
     # card = Bowling_Scorecard.new
     # expect(subject).to respond_to(:record_roll)
-    
+
     subject.record_roll(6)
     expect(subject.total_score).to eq 6
   end
-  
+
   describe 'frames' do
-    it "can record a frame and track its score" do
+    it 'can record a frame and track its score' do
       subject.record_roll(6)
       subject.record_roll(2)
       subject.record_roll(3)
@@ -20,28 +22,28 @@ describe Bowling_Scorecard do
       expect(subject.frame_score(2)).to eq 11
       expect(subject.frame_score(3)).to eq 1
     end
-    
+
     describe '10th frame' do
-      it "ends game if no strike or spare after two rolls" do
+      it 'ends game if no strike or spare after two rolls' do
         18.times { subject.record_roll(3) } # first 9 frames
         subject.record_roll(3)
         subject.record_roll(3)
         expect(subject.frame_score(10)).to eq 6
-        expect{ subject.record_roll(4) }.to raise_error "Game is finished!"
+        expect { subject.record_roll(4) }.to raise_error 'Game is finished!'
       end
-      it "provides a 3 roll frame with two bonus rolls if first roll is a strike" do
+      it 'provides a 3 roll frame with two bonus rolls if first roll is a strike' do
         18.times { subject.record_roll(3) } # first 9 frames
         subject.record_roll(10)
         subject.record_roll(3)
         subject.record_roll(6)
         expect(subject.frame_score(10)).to eq 19
-        expect{ subject.record_roll(4) }.to raise_error "Game is finished!"
+        expect { subject.record_roll(4) }.to raise_error 'Game is finished!'
       end
     end
-  end 
+  end
   describe 'bonuses' do
-    describe "strikes" do
-      it "records a strike and updates frame accordingly" do
+    describe 'strikes' do
+      it 'records a strike and updates frame accordingly' do
         subject.record_roll(10)
         expect(subject.frame).to eq 2
         subject.record_roll(6)
@@ -49,7 +51,7 @@ describe Bowling_Scorecard do
         subject.record_roll(10)
         expect(subject.frame).to eq 4
       end
-      it "adds bonus points from next two rolls" do
+      it 'adds bonus points from next two rolls' do
         subject.record_roll(10)
         subject.record_roll(2)
         subject.record_roll(3)
@@ -57,8 +59,8 @@ describe Bowling_Scorecard do
         expect(subject.frame_score(1)).to eq 15
       end
     end
-    describe "spares" do
-      it "processes spare and adds bonus points of next roll" do
+    describe 'spares' do
+      it 'processes spare and adds bonus points of next roll' do
         subject.record_roll(3)
         subject.record_roll(7)
         subject.record_roll(3)
@@ -69,7 +71,6 @@ describe Bowling_Scorecard do
         subject.record_roll(4)
         expect(subject.frame_score(3)).to eq 14
       end
-
     end
   end
 
@@ -87,5 +88,4 @@ describe Bowling_Scorecard do
       expect(output).to eq 'Gutter Game!'
     end
   end
-
 end
